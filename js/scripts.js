9ac9b2487b131fe6fdd8bed8eb0b9df5d55623c7
orbu13 <orelbutbul13@gmail.com>
2024-03-09 14:59:32 -0500

    practicing

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//Example changing elements
+let arrayA = [18,29,30,32]
+arrayA[0] = 20
+
+let arrayB = [34,65,2]
+arrayB[2] = 9
+
+//Example changing elements
+let arrayC = [43,54,33,[8765, 866,3],[34,33,2,0],0]
+let myDate = arrayC[2][3]
+
+let ourArray = [" Stimpson", "j", "banana"];
+ourArray.push["happy", "joy"]
+
+let myArray = [["John, 23"]["vcat", 59]];
+myArray.push(["dog", 30])
